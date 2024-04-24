6d4df4eb024cfe491655a909ccb5f1697944573b
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-24 09:58:44 -0600

    Refactor hook by removing the mode state, and set the mode property to be equal to history[history.length - 1]

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
+  const [history, setHistory] = useState([initial]); 
 
-  function transition(newMode) {
-    setMode(newMode);
+  function transition(newMode){
     setHistory(prev => [...prev, newMode])
   }
+
   function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
   }
 
   return { 
-    mode,
-    transition,
-    back,
-   };
-};
+    mode: history[history.length -1], 
+    transition, 
+    back };
+}
